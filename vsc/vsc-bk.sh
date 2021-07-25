0afc7d2e5c635a5901e63304222ebd9837f024f2
re4388 <re4388@gmail.com>
2021-07-25 08:41:40 +0800

    add vsc bk

---
+++ vsc/vsc-bk.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+powershell.exe -File "C:\Users\re438\AppData\scripts\test1.ps1" > win-vsc-ext.txt
+cp /mnt/c/Users/re438/AppData/Roaming/Code/User/settings.json ./
+cp /mnt/c/Users/re438/AppData/Roaming/Code/User/keybindings.json ./
