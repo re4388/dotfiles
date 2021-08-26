8d1e51ff5bad2b946bb0029573ef8433d8da51f4
re4388 <re4388@gmail.com>
2021-08-26 09:05:39 +0800

    update

---
+++ quteBrowser/js/test.js
@@ -0,0 +1,19 @@
+const doneText = `
+RAI-790
+merge
+
+RAI-2873
+merge
+
+RAI-3114
+begin to work on, doc finished
+`;
+
+const todoText = `
+RAI-3114
+continue.
+`;
+
+// TODO this shall be auto-gen from above text
+// const issues = 'RAI-3114 RAI-790 RAI-2873';
+console.log('22');
