667cc752325036c23e30e895e8e83813f49dc49f
re4388 <re4388@gmail.com>
2021-08-31 13:09:44 +0800

    update notice change from wits

--- notice_change.sh
+++ notice_change.sh
@@ -12,18 +12,12 @@ function git_pull(){
     cd ${local_dotfiles_path} && git pull && cd -
 }
 
-# function open_lazygit(){
-#     cd ${local_dotfiles_path} && lazygit
-# }
 
 function update_ahk(){
 		cp ${local_dotfiles_path}/autohotkey/ben.ahk /mnt/c/ahk/ben.ahk
 }
 
 
-# function bk_wsl_vim(){
-#     cp -rf ~/.config/nvim ${local_dotfiles_path}/nvim-ver/wsl/vim-IDE-ver/
-# }
 
 function update_win10_vim(){ 
     cp ${local_dotfiles_path}/nvim-ver/win10/vscode-nvim/init.vim /mnt/c/Users/${1}/AppData/Local/nvim/
@@ -33,9 +27,6 @@ function update_wsl_vim(){
     cp -rf ${local_dotfiles_path}/nvim-ver/wsl/vim-IDE-ver/nvim ~/.config/
 }
 
-# function bk_win10_vim(){
-#     cp "/mnt/c/Users/${1}/AppData/Local/nvim/init.vim" "${local_dotfiles_path}/nvim-ver/win10/vscode-nvim/"
-# }
 
 function updateQuteBrowser(){
     cp "${local_dotfiles_path}/quteBrowser/config.py" "/mnt/c/Users/${1}/AppData/Roaming/qutebrowser/config/config.py"
@@ -46,22 +37,10 @@ function updateQuteBrowser(){
     cp -rf "${local_dotfiles_path}/quteBrowser/userscripts" "/mnt/c/Users/${1}/AppData/Roaming/qutebrowser/config"
 }
 
-# function bkQuteBrowser(){
-#     sudo cp "/mnt/c/Users/${1}/AppData/Roaming/qutebrowser/config/config.py" "${local_dotfiles_path}/quteBrowser"
-#     sudo cp "/mnt/c/Users/${1}/AppData/Roaming/qutebrowser/config/quickmarks" "${local_dotfiles_path}/quteBrowser"
-#     # note: when use cp -rf, it copy over the "js" folder into taget path, so the target path shall be at the parent path 
-#     sudo cp -rf "/mnt/c/Users/${1}/AppData/Roaming/qutebrowser/config/js" "${local_dotfiles_path}/quteBrowser"
-#     sudo cp -rf "/mnt/c/Users/${1}/AppData/Roaming/qutebrowser/config/greasemonkey" "${local_dotfiles_path}/quteBrowser"
-#     sudo cp -rf "/mnt/c/Users/${1}/AppData/Roaming/qutebrowser/config/userscripts" "${local_dotfiles_path}/quteBrowser"
-
-# }
-
-# function bk_ahk(){
-#     sudo cp "/mnt/c/ahk/ben.ahk" "${local_dotfiles_path}/autohotkey/"
-# }
 
 
 
+# no need to display ls result after cd, so here we use 1>/dev/null to not display it 
 cd ${local_dotfiles_path} 1>/dev/null
 git fetch
 git diff ...origin/main > $File
