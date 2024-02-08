0678d47feaffca871dd0fb0c49af9e8b0c918cd6
re4388 <re4388@gmail.com>
2024-02-08 15:24:39 +0800

    WIP

---
+++ zsh/custom_alias.zsh
@@ -0,0 +1,26 @@
+
+alias vi=nvim
+alias vim=lvim
+
+
+alias kc=kubectl
+alias minik=minikube
+alias kctx=kubectx
+
+
+# IDEA open, like `code` for vscode
+alias idea='open -na "IntelliJ IDEA.app" --args'
+alias goland='open -na "GoLand.app" --args'
+alias du='gdu-go'
+
+alias fdf="fd | fzf"
+
+
+alias ls="eza"
+alias ll="eza -al"
+
+
+alias qq='ben'
+
+alias j="z"
+alias lg="Lazygit"
