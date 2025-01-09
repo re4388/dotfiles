d271fa9fd99aa799e2d20e0d146a3705258e7954
re4388 <re4388@gmail.com>
2025-01-09 18:45:16 +0800

    2025_01_09_18_45_14

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -355,3 +355,6 @@ esac
 
 
 # PATH=~/.console-ninja/.bin:$PATH
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
--- zsh/__wemo.zsh
+++ zsh/__wemo.zsh
@@ -1,28 +1,20 @@
-
 function wemosql() {
-#  alias gcsp='bash -c '\''cd /Users/josh/Documents/wemo/gcp/local-cloudsql-proxy && bash launchV2.sh josh.tsai $@'\'' _'
+    #  alias gcsp='bash -c '\''cd /Users/josh/Documents/wemo/gcp/local-cloudsql-proxy && bash launchV2.sh josh.tsai $@'\'' _'
     cd /Users/re4388/project/work/local-cloudsql-proxy
     echo "${wemo_account_pass}" | pbcopy
-    /Users/re4388/project/work/local-cloudsql-proxy/launchV2.sh ben.hu prod qat
+    /Users/re4388/project/work/local-cloudsql-proxy/launchV2.sh ben.hu prod qat backup
 }
 
-
-
-
 function wemosqlqat() {
-#  alias gcsp='bash -c '\''cd /Users/josh/Documents/wemo/gcp/local-cloudsql-proxy && bash launchV2.sh josh.tsai $@'\'' _'
+    #  alias gcsp='bash -c '\''cd /Users/josh/Documents/wemo/gcp/local-cloudsql-proxy && bash launchV2.sh josh.tsai $@'\'' _'
     cd /Users/re4388/project/work/local-cloudsql-proxy
     echo "${wemo_account_pass}" | pbcopy
     /Users/re4388/project/work/local-cloudsql-proxy/launchV2.sh ben.hu qat
 }
 
 function wemosqlprod() {
-#  alias gcsp='bash -c '\''cd /Users/josh/Documents/wemo/gcp/local-cloudsql-proxy && bash launchV2.sh josh.tsai $@'\'' _'
+    #  alias gcsp='bash -c '\''cd /Users/josh/Documents/wemo/gcp/local-cloudsql-proxy && bash launchV2.sh josh.tsai $@'\'' _'
     cd /Users/re4388/project/work/local-cloudsql-proxy
     echo "${wemo_account_pass}" | pbcopy
     /Users/re4388/project/work/local-cloudsql-proxy/launchV2.sh ben.hu prod
 }
-
-
-
-
